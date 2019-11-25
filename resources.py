1af6305b7dcc61919a09ed5fe6f3fdf16cfb6108
dp.sokolov <dp.sokolov@jet.su>
2019-11-25 20:18:55 +0300

    update server logic, update gitignore

--- resources.py
+++ resources.py
@@ -1,9 +1,5 @@
-from collections import defaultdict, namedtuple
-
+import random
 from proto import leaderboard_pb2
-
-my_leader_board = defaultdict(tuple)
-Score = namedtuple('Score', ['score', 'rank'])
 
 
 def setup_database():
@@ -19,17 +15,10 @@ def get_token(db_connection, login_password):
 
 
 def store_player_score(db_connection, player_score):
-    global my_leader_board
 
     print(f'store player score: {player_score.name} {player_score.score}')
 
-
-
-    my_leader_board.append((player_score.name, player_score))
-    my_leader_board.sort(key=lambda x: x[1])
-    rank = my_leader_board.index()
-
-    return leaderboard_pb2.ScoreResponse(name=player_score.name, rank=rank)
+    return leaderboard_pb2.ScoreResponse(name=player_score.name, rank=random.randint(1, 100))
 
 
 def get_leaderboard(db_connection, get_lb):
