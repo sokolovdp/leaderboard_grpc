b6e3e47d7b27f670218a22379bae7bc1d369e0ae
dp.sokolov <dp.sokolov@jet.su>
2019-12-01 00:41:45 +0300

    refactoring and typing

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ GRPC_SERVER_PORT = os.getenv('GRPC_SERVER_PORT', '[::]:50051')
 
 REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 GAME_NAME = os.getenv('GAME_NAME', 'dimas')
-PASSWORD_SALT = os.getenv('PASSWORD_HASH', 'quick fox jumps over the lazy dog')
+PASSWORD_SALT = os.getenv('PASSWORD_HASH', 'Quick brown fox jumps over the lazy dog')
 LEADERBOARD_PAGE_SIZE = os.getenv('LEADERBOARD_PAGE_SIZE', 3)
 
 LEADERBOARD_ALL_TIMES = GAME_NAME + 'leaderboard'
