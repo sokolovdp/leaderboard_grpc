0aa94449d12b375164e5051009b673b543370c5d
dp.sokolov <dp.sokolov@jet.su>
2019-12-03 13:03:39 +0300

    finalize docker-compose and file structure

--- cron_script.sh
+++
@@ -1,3 +0,0 @@
-touch /var/spool/cron/leaderboard
-/usr/bin/crontab /var/spool/cron/leaderboard
-echo "0 3 * * * cd /home/dmitrii/leaderboard_grpc && cron_job.py >> ~/cron.log" >> /var/spool/cron/leaderboard
